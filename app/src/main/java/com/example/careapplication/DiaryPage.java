57e8cad7b0dc3a0f94365c5314a0e054c7144f8d
ChoiYoungHan <dnflwlq373@naver.com>
2023-03-26 22:27:00 +0900

    캘린더에서의 클릭 이벤트 처리와 갤러리 접근

--- app/src/main/java/com/example/careapplication/DiaryPage.java
+++ app/src/main/java/com/example/careapplication/DiaryPage.java
@@ -2,13 +2,60 @@ package com.example.careapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DiaryPage extends AppCompatActivity {
+
+    // TextView를 사용하기 위한 변수 선언
+    private TextView Tv_Date;
+
+    // Button을 사용하기 위한 변수 선언
+    private Button Btn_GetImage;
+
+    // 갤러리에서 이미지를 선택하는 요청에 대한 결과를 식별하기 위한 상수값 선언
+    // 상수값은 아무 숫자나 상관이 없지만, 요청 코드가 다른 코드에서 중복되지 않도록 해야한다.
+    private static final int REQUEST_CODE_PICK_IMAGE = 100;
+
+    // 갤러리에서 사진을 가져오기 위한 메소드
+    private void pickImage() {
+        // 갤러리 호출하는 intent 생성 (ACTION_GET_CONTENT는 안드로이드에서 파일을 선택하고 불러오는 데 사용하는 상수이다)
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*"); // 선택 가능한 파일의 종류를 image로 한다.
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); // 여러 개의 이미지를 선택할 수 있도록 EXTRA_ALLOW_MULTIPLE을 true 로 설정
+
+        // startActivityForResult의 경우 최신 버전이 나옴에 따라 사용이 권장되지 않음
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_CODE_PICK_IMAGE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diary_page);
+        setContentView(R.layout.activity_diary_page); // activity_diary.xml에 설정된 View들을 가져옴
+
+        // activity_diary.xml에서 설정된 View를 가져와서 정의
+        Tv_Date = findViewById(R.id.Tv_Date);
+
+        Btn_GetImage = findViewById(R.id.Btn_GetImage);
+
+        // Diary_calendarPage에서 전달받은 데이터 추출
+        Bundle bundle = getIntent().getExtras();
+        int year = bundle.getInt("year");
+        int month = bundle.getInt("month");
+        int day = bundle.getInt("day");
+
+        Tv_Date.setText(year + "년 " + month + "월 " + day + "일");
+
+        Btn_GetImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/careapplication/Diary_calendarPage.java
+++ app/src/main/java/com/example/careapplication/Diary_calendarPage.java
@@ -3,19 +3,22 @@ package com.example.careapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
 
 public class Diary_calendarPage extends AppCompatActivity {
 
-
+    private long ClickTime = 0; // 캘린더에서 첫 번째로 클릭한 시간을 저장할 변수 선언
+    private int before_Year, before_Month, before_Day; // 캘린더에서 첫 번째로 클릭한 날짜의 값을 저장할 변수 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diary_calendar_page);
+        setContentView(R.layout.activity_diary_calendar_page); // activity_diary_calendar.xml에서 설정된 View들을 가져옴
 
+        // activity_diary_calendar.xml에서 설정된 View들을 가져와서 정의
         CalendarView calendarView = findViewById(R.id.Calendar);
 
         // setOnDateChangeListener는 캘린더에서 날짜가 선택될 때 호출되는 메소드를 등록하는 역할
@@ -24,8 +27,26 @@ public class Diary_calendarPage extends AppCompatActivity {
             // onSelectedDayChange는 선택된 날짜의 정보를 인자로 전달해주는 역할
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                // 선택된 날짜의 정보가 잘 받아와 지는지 Log로 출력
-                Log.d("Tag", "오늘은 " + year + "년 " + (month + 1) + "월 " + day + "일 입니다.");
+
+                long now = System.currentTimeMillis(); // 현재 시간을 가져와서 now에 저장
+
+                // 이전에 클릭한 날짜와 현재 클릭한 날짜의 값을 비교하여 일치하는지 확인
+                if(before_Year == year && before_Month == month && before_Day == day){
+                    if(now - ClickTime < 500){ // 500ms(0.5초) 이내에 다시 클릭되었는지 확인한다.
+                        Intent intent = new Intent(Diary_calendarPage.this, DiaryPage.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("year", year);
+                        bundle.putInt("month", (month+1));
+                        bundle.putInt("day", day);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                } else {
+                    before_Year = year;
+                    before_Month = month;
+                    before_Day = day;
+                    ClickTime = now;
+                }
             }
         });
     }
